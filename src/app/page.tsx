bb85ecae8c84c81b1c76408268a7dab95bd049e0
Arsalan23c <Arsalan23c@gmail.com>
2025-01-10 14:56:59 +0500

    Done

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Carsoul from "./components/Carsoul";
+import Products from "./product/page";
+import Contact from "./Contact/page"
+
+
+export default function Home() {
+  return (
+    <>
+   <Carsoul/>
+   <Products/>
+   <Contact/>
+
+
+    
+    </>
+  );
+}
